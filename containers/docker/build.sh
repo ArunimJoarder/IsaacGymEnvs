c13c8a4c4b15bec5b975b929573489e4fcebc53e
ArunimJoarder <ajoarder@ethz.ch>
2023-12-14 16:36:57 +0100

    edits to container defs

--- containers/docker/build.sh
+++ containers/docker/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-home=`realpath "$(dirname "$0")"/../../../../`
+home=`realpath "$(dirname "$0")"/../../../`
 cd $home && sudo docker build --network=host --memory-swap -1 -m 20g -t dextreme-thesis -f IsaacGymEnvs/isaacgymenvs/containers/docker/Dockerfile .
