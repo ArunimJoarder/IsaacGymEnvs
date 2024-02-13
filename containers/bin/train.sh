add3b8d02342e583098ae8b557b75c51bd550bd0
ArunimJoarder <ajoarder@ethz.ch>
2024-02-13 14:54:47 +0100

    absolute path for model saving

--- containers/bin/train.sh
+++ containers/bin/train.sh
@@ -7,24 +7,24 @@
 # --warp: run command in ''. Does not work with variables (that's why run.sh is needed).
 
 export run_cmd="python3 /deXtreme/IsaacGymEnvs/isaacgymenvs/train.py \
-                  task=AllegroHandDextremeAdversarialObservations \
-                  base_checkpoint=exported_models/AllegroHandADR.onnx \
+                  task=AllegroHandDextremeADRFinetuning \
+                  task.onnx_noise_gen_checkpoint=exported_models/AllegroHandAdversarialObservationsAndActions.onnx \
                   wandb_activate=True \
                   wandb_entity=joarder-arunim-github \
                   headless=True \
                   num_envs=8192 \
-                  experiment=train_adv_obs_cube_pos_0.05_cube_rot_0.1_no_dof_pos_obs \
+                  experiment=euler_finetuning \
                   task.experiment_dir=while_training \
                   task.env.printNumSuccesses=True"
 
 export custom_flags="--nv --writable -B /cluster/home/$USER/git/IsaacGymEnvs/:/deXtreme/IsaacGymEnvs"
 
 sbatch \
-  -n 16 \
-  --mem-per-cpu=4096 \
+  -n 4 \
+  --mem-per-cpu=8192 \
   -G 1 \
-  --gres=gpumem:10240 \
-  --time=12:00:00 \
+  --gres=gpumem:204800 \
+  --time=4:00:00 \
   -A es_hutter \
   --mail-type=END \
   --tmp=15G \
