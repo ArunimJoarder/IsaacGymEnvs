72f9075de967bcb13f6c12e8ac939c48a7da6a68
ArunimJoarder <ajoarder@ethz.ch>
2024-03-01 11:40:47 +0100

    different rewards for delta delta action penalty

--- containers/bin/train.sh
+++ containers/bin/train.sh
@@ -6,19 +6,33 @@
 # --mail-type=END : Triggers email to $USER@ethz.ch at the end of training
 # --warp: run command in ''. Does not work with variables (that's why run.sh is needed).
 
+# export run_cmd="python3 /deXtreme/IsaacGymEnvs/isaacgymenvs/train.py \
+#                   task=AllegroHandDextremeADRFinetuningResidualActions \
+#                   task.onnx_noise_gen_checkpoint=exported_models/AllegroHandAdversarialObservationsAndActions.onnx \
+#                   base_checkpoint=exported_models/AllegroHandADR.onnx \
+#                   wandb_activate=True \
+#                   wandb_entity=joarder-arunim-github \
+#                   headless=True \
+#                   num_envs=4096 \
+#                   experiment=euler_ft_delta_adv_prob_0.00_-1.0_action_penalty_-20_delta_action_penalty_adr_ranges_4096_5000_iters \
+#                   task.experiment_dir=while_training \
+#                   task.env.printNumSuccesses=True \
+#                   task.env.adv_noise_prob=0.00 \
+#                   task.task.adr.adr_load_from_checkpoint=True \
+#                   max_iterations=10000"
+
 export run_cmd="python3 /deXtreme/IsaacGymEnvs/isaacgymenvs/train.py \
                   task=AllegroHandDextremeADRFinetuningResidualActions \
                   task.onnx_noise_gen_checkpoint=exported_models/AllegroHandAdversarialObservationsAndActions.onnx \
                   base_checkpoint=exported_models/AllegroHandADR.onnx \
-                  wandb_activate=True \
-                  wandb_entity=joarder-arunim-github \
                   headless=True \
                   num_envs=4096 \
-                  experiment=euler_ft_delta_adv_prob_0.00_4096_5000_iters \
+                  experiment=test \
                   task.experiment_dir=while_training \
-                  task.env.printNumSuccesses=True \
                   task.env.adv_noise_prob=0.00 \
-                  max_iterations=5000"
+                  task.task.adr.adr_load_from_checkpoint=True \
+                  max_iterations=10"
+
 
 export custom_flags="--nv --writable -B /cluster/home/$USER/git/IsaacGymEnvs/:/deXtreme/IsaacGymEnvs"
 
@@ -27,7 +41,7 @@ sbatch \
   --mem-per-cpu=8192 \
   -G 1 \
   --gres=gpumem:204800 \
-  --time=10:00:00 \
+  --time=01:00:00 \
   -A es_hutter \
   --mail-type=END \
   --tmp=15G \
